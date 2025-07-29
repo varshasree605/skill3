afeaea39e38c30799078b7d46f07b7c179016230
varshasree605 <varshasree605@gmail.com>
2025-07-29 21:09:51 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: '#282c34', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo" style={{ height: '40vmin', pointerEvents: 'none' }} /> 
+        <p style={{ color: '#61dafb', fontsize: '24px' }}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ color: '#61dafb', fontsize: '20px' }}
         >
           Learn React
         </a>
